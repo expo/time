7b24d3a4dbd24544218367cc5db072a24fcc1642
Charlie Cheever <ccheever@gmail.com>
2018-08-20 15:17:41 +0100

    Adding usage and making the module

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # time
 Simple JS timer with logging
+
+Usage:
+
+```
+
+let time = require('@expo/time');
+
+async function getUrlAsync(url) {
+
+  let tk == time.start();
+  let response = fetch(url);
+  let responseText = await response.text();
+  tk.stop(tk, 'fetch', {message: url, threshold: 10});
+  return responseText;
+
+}
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+let _START_TIMES = {};
+
+function start() {
+  let key = Symbol('time');
+  _START_TIMES[key] = Date.now();
+  return key;
+}
+
+function end(key, label, opts) {
+  let endTime = Date.now();
+  opts = opts || {};
+  let startTime = _START_TIMES[key];
+  if (!startTime) {
+    throw new Error('No recorded start time for key');
+  }
+  let t = endTime - startTime;
+  let threshold = opts.threshold || -1;
+  let message = opts.message || '';
+  if (t > threshold) {
+    console.log(label + ': ' + t + 'ms ' + message);
+  }
+  delete _START_TIMES[key];
+  return t;
+}
+
+module.exports = {
+  start,
+  end,
+};
